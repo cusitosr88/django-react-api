09041005cead7441362e7138b28b92d92a0cdbe6
Javier <cusitosr88@gmail.com>
2022-03-05 10:31:48 -0500

    feat: User serializer.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -2,6 +2,17 @@ from dataclasses import fields
 from pyexpat import model
 from rest_framework import serializers
 from api.models import Movie, Rating
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password']
+        extra_kwargs = {'password': {'write_only': True, 'required': True}}
+    
+    def create(self, validate_data):
+        user =  User.objects.create(**validate_data)
+        return user
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
