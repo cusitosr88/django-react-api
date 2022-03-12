b24caf4a24848715fd92b506fbbe7d43064532a6
Javier <cusitosr88@gmail.com>
2022-03-12 10:58:59 -0500

    feat: show rating.

--- frontend/src/components/movie-detail.js
+++ frontend/src/components/movie-detail.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from '@fortawesome/free-solid-svg-icons'
 
 function MovieDetail(props) {
 
+    const mov = props.movie
+
     return(
         <div>
-            { props.movie ? (
+            { mov ? (
                 <div>
-                    <h1>{props.movie && props.movie.name}</h1>
-                    <p>{props.movie && props.movie.description}</p>
+                    <h1>{mov && mov.name}</h1>
+                    <p>{mov && mov.description}</p>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 0 ? 'orange' : 'no-review'}/>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 1 ? 'orange' : 'no-review'}/>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 2 ? 'orange' : 'no-review'}/>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 3 ? 'orange' : 'no-review'}/>
+                    <FontAwesomeIcon icon={faStar} className={mov.avg_rating > 4 ? 'orange' : 'no-review'}/>
+                    ({mov.avg_rating})
                 </div>
             ) : null}
         </div>
